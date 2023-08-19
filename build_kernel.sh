fde7d27977b940be2af653637bc696e1faba5e5e
Nikolay1257 <otraskinnikolaj.com>
2023-08-19 11:17:21 +0300

    upload file

---
+++ build_kernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export CROSS_COMPILE=$(pwd)/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export ARCH=arm64
+export ANDROID_MAJOR_VERSION=r
+
+export KCFLAGS=-w
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a12_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j4
+
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
